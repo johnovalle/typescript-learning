de287f04e235507be32fcc968ed7af80bae8c545
John Ovalle <johnovalle@gmail.com>
2017-09-08 19:22:48 -0400

    bootstrap library app

--- libraryManager/app.ts
+++ libraryManager/app.ts
@@ -1,7 +1,42 @@
-class something2 {
-    constructor(){}
+export const getAllBooks = () => {
+    let books = [
+        {title: "U", author: "JJ", available: true, category: Category.Fiction },
+        {title: "GoT", author: "GRRM", available: false, category: Category.Fiction },
+        {title: "AFtA", author: "EH", available: true, category: Category.History }
+    ]
+    return books;
 }
 
-const test2 = "testing visual studio code debugger";
+const logFirstAvailable = (books): void => {
+    const numberOfBooks: number = books.length;
+    let firstAvailable: string = '';
 
-console.log("Watching");
+    for(let currentBook of books) {
+        
+        if(currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log(`Total: ${numberOfBooks}`);
+    console.log(`first: ${firstAvailable}`);
+}
+
+enum Category { Biography, Poetry, Fiction, History, Children };
+
+const getBookTitlesByCategory = (categoryFilter: Category): Array<string> => {
+    console.log(`Getting ${Category[categoryFilter]} books`);
+    const allBooks = getAllBooks();
+    const filteredTitles: string[] = [];
+
+    for(let currentBook of allBooks) {
+        if(currentBook.category === categoryFilter){
+            filteredTitles.push(currentBook.title);
+        }
+    }
+
+    return filteredTitles;
+};
+
+
+console.log(getBookTitlesByCategory(Category.Fiction));
--- libraryManager/js/app.js
+++ libraryManager/js/app.js
@@ -1,8 +1,46 @@
-var something2 = /** @class */ (function () {
-    function something2() {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAllBooks = function () {
+    var books = [
+        { title: "U", author: "JJ", available: true, category: Category.Fiction },
+        { title: "GoT", author: "GRRM", available: false, category: Category.Fiction },
+        { title: "AFtA", author: "EH", available: true, category: Category.History }
+    ];
+    return books;
+};
+var logFirstAvailable = function (books) {
+    var numberOfBooks = books.length;
+    var firstAvailable = '';
+    for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+        var currentBook = books_1[_i];
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
     }
-    return something2;
-}());
-var test2 = "testing visual studio code debugger";
-console.log("Watching");
+    console.log("Total: " + numberOfBooks);
+    console.log("first: " + firstAvailable);
+};
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+;
+var getBookTitlesByCategory = function (categoryFilter) {
+    console.log("Getting " + Category[categoryFilter] + " books");
+    var allBooks = exports.getAllBooks();
+    var filteredTitles = [];
+    for (var _i = 0, allBooks_1 = allBooks; _i < allBooks_1.length; _i++) {
+        var currentBook = allBooks_1[_i];
+        if (currentBook.category === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+    return filteredTitles;
+};
+console.log(getBookTitlesByCategory(Category.Fiction));
 //# sourceMappingURL=app.js.map
