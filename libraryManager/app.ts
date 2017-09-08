28b1b47343f5e1d45afb84164336fa55fc8925ec
John Ovalle <johnovalle@gmail.com>
2017-09-08 18:51:52 -0400

    visual studio code configurations

---
+++ libraryManager/app.ts
@@ -0,0 +1,7 @@
+class something2 {
+    constructor(){}
+}
+
+const test2 = "testing visual studio code debugger";
+
+console.log("Watching");
---
+++ libraryManager/js/app.js
@@ -0,0 +1,8 @@
+var something2 = /** @class */ (function () {
+    function something2() {
+    }
+    return something2;
+}());
+var test2 = "testing visual studio code debugger";
+console.log("Watching");
+//# sourceMappingURL=app.js.map
