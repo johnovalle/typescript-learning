7724a35a50bc415b34741828e1f858ae6c9b9949
John Ovalle <johnovalle@gmail.com>
2017-09-15 15:56:56 -0400

    remove .js files from tracking

--- libraryManager/js/utilityFunctions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Purge(inventory) {
-    return inventory.splice(2, inventory.length);
-}
-exports.Purge = Purge;
-//# sourceMappingURL=utilityFunctions.js.map
